aa5faeb8aa33f3d08e8ba3d519b9f5b245c61efc
caneryild <caneryild163@gmail.com>
2016-04-19 16:41:51 +0300

    Update ProteinSequenceWeighedSimilarity.py

--- ProteinSequenceWeighedSimilarity.py
+++ ProteinSequenceWeighedSimilarity.py
@@ -1,1 +1,7 @@
-
+def similarityOfProteins(protein1,protein2):
+  lcs=largestCommonSubsequence(protein1,protein2)
+        ' find the first of largest common subseqence in each of them
+        'and compare them from the first index through last one 
+        'and compare them from the first index of L.C.S. to the ends.
+        'the reverse for 2 two steps are repeated from end to start and from end of lcs.'s to start.
+        'Total of m*n*2 comparisions are made.
