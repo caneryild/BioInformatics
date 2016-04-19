e264449a960e91c0d27c519b2bd2f356d60a0daf
caneryild <caneryild163@gmail.com>
2016-04-19 18:22:37 +0300

    Create HuffmanCoding.py

---
+++ HuffmanCoding.py
@@ -0,0 +1,7 @@
+#proteins have alpha helix or beta helix structures and d/rna have many repeats, 
+#so creating a dictionary giving encoded element and its decoded version
+#compressing any data into coded version and finding out repating seuences
+
+def findRepeats(sequence,length):
+  codes=0
+  repeats=0
